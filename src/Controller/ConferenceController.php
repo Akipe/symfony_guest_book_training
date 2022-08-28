6e259c427957adb1147948251cd37de237aa831e
Julien Milletre Akipe <code.julien@milletre.fr>
2022-08-28 17:56:36 +0200

    Add easter egg

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $name = $request->query->get('hello');
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'ConferenceController',
+            'name' => htmlspecialchars($name)
         ]);
     }
 }
